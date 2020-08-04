d60f9e961cabfd6b6a1e36fbdaea4f628eac817d
Andre Emidio <andre@datatype.com.br>
2020-08-04 00:51:13 -0300

    Correção do arquivo teste.py

--- teste.py
+++ teste.py
@@ -1,17 +1,29 @@
 import numpy as np      # noqa
 import cv2
 from matplotlib import pyplot as plt
+import imutils
 
 image =  'PLGlnWj.png'
 img = cv2.imread(image)
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgray =  imgray * 255
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 
-kernel = np.ones((7,7),np.uint8)
-dilation = cv2.dilate(img,kernel,iterations = 10)
 
-edges = cv2.Canny(img,200,300)
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img, contours, -1, (0,255,0), 1)
-plt.imshow(edges)
-plt.imshow(img)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6,6))
+dilation = cv2.dilate(thresh, kernel, iterations=1 )
+
+edges = cv2.Canny(dilation,150,200)
+contours, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+for contour in contours:
+    try:
+        M = cv2.moments(contour)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+    except ZeroDivisionError:
+        pass
+
+    contornos = cv2.drawContours(img, [contour], -1, (0,255,0), 1)
+    cv2.circle(img, (cX, cY), 1, (255,255,255), -1)
+    cv2.imwrite('imagem_resultado.png', img)
+    plt.imshow(img)
